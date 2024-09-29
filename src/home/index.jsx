fb4a5c153508bb231e51125cfaeff8ba94d74c2d
Rahul Yadav <98537578+therahulyadav@users.noreply.github.com>
2024-09-29 10:50:16 +0530

    Update index.jsx in home

--- src/home/index.jsx
+++ src/home/index.jsx
@@ -22,7 +22,7 @@ function Home() {
                    <Link to={'/dashboard'}>      <div className="inline-flex justify-center items-center py-3 px-5 text-base font-medium text-center text-white rounded-lg bg-primary hover:bg-primary focus:ring-4 focus:ring-primary-300 dark:focus:ring-primary-900">
                 Get Started
                 <svg className="ml-2 -mr-1 w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
-            </div></link>
+            </div></Link>
         </div>
         <div className="px-4 mx-auto text-center md:max-w-screen-md lg:max-w-screen-lg lg:px-36">
             <span className="font-semibold text-gray-400 uppercase">FEATURED IN</span>
