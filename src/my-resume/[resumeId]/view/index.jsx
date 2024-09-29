521d7121aac8fbd3726b9083773c77d8db9e8fa9
Rahul Yadav <98537578+therahulyadav@users.noreply.github.com>
2024-09-29 14:33:38 +0530

    Update index.jsx

--- src/my-resume/[resumeId]/view/index.jsx
+++ src/my-resume/[resumeId]/view/index.jsx
@@ -42,7 +42,7 @@ function ViewResume() {
                 <p className='text-center text-gray-400'>Now you are ready to download your resume and you can share unique 
                     resume url with your friends and family </p>
             <div className='flex justify-between px-44 my-10'>
-                <Button onClick={HandleDownload}>Download</Button>
+{/*                 <Button onClick={HandleDownload}>Download</Button>
                
                 <RWebShare
         data={{
@@ -52,14 +52,14 @@ function ViewResume() {
         }}
         onClick={() => console.log("shared successfully!")}
       > <Button>Share</Button>
-      </RWebShare>
+      </RWebShare> */}
             </div>
         </div>
             
         </div>
         <div className='my-10 mx-10 md:mx-20 lg:mx-36'>
         <div id="print-area" >
-{/*                 <ResumePreview/> */}
+                <ResumePreview/>
             </div>
             </div>
     </ResumeInfoContext.Provider>
