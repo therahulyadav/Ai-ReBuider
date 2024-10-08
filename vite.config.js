14edf296de92d2bace9283de987369eaaefa46e6
Rahul Yadav <98537578+therahulyadav@users.noreply.github.com>
2024-10-08 23:43:51 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,12 @@
-import path from "path"
-import react from "@vitejs/plugin-react"
-import { defineConfig } from "vite"
+import path from "path";
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
 
 export default defineConfig({
   plugins: [react()],
   build: {
     rollupOptions: {
-      external: ['file-saver'], ['html-to-docx'], // Externalize file-saver to avoid issues
+      external: ['file-saver', 'html-to-docx'], // Corrected the array syntax
     },
   },
   resolve: {
@@ -14,4 +14,4 @@ export default defineConfig({
       "@": path.resolve(__dirname, "./src"),
     },
   },
-})
+});
