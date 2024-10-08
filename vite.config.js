174cd8831bbaec9050edca0d0509f65dd297c305
Rahul Yadav <98537578+therahulyadav@users.noreply.github.com>
2024-10-09 02:02:58 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,20 +1,12 @@
-import path from 'path';
-import react from '@vitejs/plugin-react';
-import { defineConfig } from 'vite';
+import path from "path"
+import react from "@vitejs/plugin-react"
+import { defineConfig } from "vite"
 
 export default defineConfig({
   plugins: [react()],
-   build: {
-    rollupOptions: {
-      external: ['html-docx-js'],  // Externalize 'html-docx-js'
-    },
-  },
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, './src'),
+      "@": path.resolve(__dirname, "./src"),
     },
   },
-  optimizeDeps: {
-    include: ['html-docx-js'],  // Pre-bundle html-to-docx
-  },
-});
+})
