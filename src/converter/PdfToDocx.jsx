7370be3524b2a090107dca5cd0e75a7aad5f04e0
Rahul Yadav <therahulyadav@users.noreply.github.com>
2024-10-09 23:28:54 +0530

    added uploaded text

--- src/converter/PdfToDocx.jsx
+++ src/converter/PdfToDocx.jsx
@@ -4,6 +4,7 @@ import { Button } from "@/components/ui/button";
 
 function PdfToDocx(){
     const file = useRef();
+    const label = useRef();
     const secretKey = import.meta.env.VITE_CONVERT_API_SECRET_KEY;
     const handleDownloadDoc = async () => {
         if(file.current.files[0]){
@@ -16,12 +17,18 @@ function PdfToDocx(){
         window.open(url);
         }else{alert("Please Upload file")}
       };
+    function changeLabel(){
+        console.log(file.current.files[0]);
+        if(file.current.files[0]){
+            label.current.innerText = `${file.current.files[0].name} has been uploaded`;
+        }
+    }
     return(
         <div className='h-full w-full flex flex-col justify-center items-center '>
 
         <div className='p-20 m-5 flex flex-col items-center justify-center text-center self-center border-4 border-red-200 border-dotted md:'>
-        <label htmlFor="fileInput">Select PDF file to convert it to DOCX</label>
-        <input className="hidden" id="fileInput" type="file" name="pdfFile" ref={file}  accept="application/pdf" ></input>
+        <label ref={label} htmlFor="fileInput">Select PDF file to convert it to DOCX</label>
+        <input className="hidden" onChange={changeLabel} id="fileInput" type="file" name="pdfFile" ref={file}  accept="application/pdf" ></input>
         </div>
         <div>
         <Button onClick={handleDownloadDoc}>Download Converted Docx</Button>
