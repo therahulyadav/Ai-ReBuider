9904c145742e9df7b8eaff3a2ff56944b9f2b3ec
Rahul Yadav <therahulyadav@users.noreply.github.com>
2024-10-09 23:14:51 +0530

    fix resumeView

--- src/my-resume/[resumeId]/view/index.jsx
+++ src/my-resume/[resumeId]/view/index.jsx
@@ -45,7 +45,7 @@ function ViewResume() {
                 Congrats! Your Ultimate AI generates Resume is ready ! </h2>
                 <p className='text-center text-gray-400'>Now you are ready to download your resume and you can share unique 
                     resume url with your friends and family </p>
-            <div className='flex justify-between px-44 my-10'>
+            <div className='flex justify-between  md:px-44 my-10'>
                 <Button onClick={HandleDownload}>Download</Button> 
                 <Link to={'/pdftodocx'}>
                 <Button >Convert PDF To Docx</Button>
