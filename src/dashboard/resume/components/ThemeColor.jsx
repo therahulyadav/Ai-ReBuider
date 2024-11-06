90c02351063a9f4ba4e72ad4e294c1eb739c958c
Rahul Yadav <98537578+therahulyadav@users.noreply.github.com>
2024-11-06 00:06:08 -0800

    Update ThemeColor.jsx
    added new sets of colors

--- src/dashboard/resume/components/ThemeColor.jsx
+++ src/dashboard/resume/components/ThemeColor.jsx
@@ -13,12 +13,20 @@ import { toast } from 'sonner'
 import { motion } from 'framer-motion'
 
 function ThemeColor() {
-    const colors=[
-        "#FF5733", "#33FF57", "#3357FF", "#FF33A1", "#A133FF",
-        "#33FFA1", "#FF7133", "#71FF33", "#7133FF", "#FF3371",
-        "#33FF71", "#3371FF", "#A1FF33", "#33A1FF", "#FF5733",
-        "#5733FF", "#33FF5A", "#5A33FF", "#FF335A", "#335AFF"
+    const colors = [
+"#33FF57", "#FF33A1", "#A133FF", "#FF7133", "#71FF33",
+"#7133FF", "#3371FF", "#A1FF33", "#5733FF", "#33FF5A", 
+"#5A33FF", "#335AFF", "#1F4E79", "#333333", "#008080",
+"#2C5F2D", "#F5F5DC"
     ]
+    // older colors 
+    // const colors=[
+    //     "#FF5733", "#33FF57", "#3357FF", "#FF33A1", "#A133FF",
+    //     "#33FFA1", "#FF7133", "#71FF33", "#7133FF", "#FF3371",
+    //     "#33FF71", "#3371FF", "#A1FF33", "#33A1FF", "#FF5733",
+    //     "#5733FF", "#33FF5A", "#5A33FF", "#FF335A", "#335AFF"
+    // ]
+    
 
     const {resumeInfo,setResumeInfo}=useContext(ResumeInfoContext);
     const [selectedColor,setSelectedColor]=useState();
