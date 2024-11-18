dcd07bbdc8651943377befeae9e6fe5ec7e5122f
Rahul Yadav <98537578+therahulyadav@users.noreply.github.com>
2024-11-18 04:39:59 -0800

    Update RichTextEditor.jsx

--- src/dashboard/resume/components/RichTextEditor.jsx
+++ src/dashboard/resume/components/RichTextEditor.jsx
@@ -7,7 +7,7 @@ import { AIChatSession } from './../../../../service/AIModal';
 import { toast } from 'sonner';
 import { motion } from 'framer-motion';
 
-const PROMPT='position titile: {positionTitle} , Depends on position title give me 5-7 bullet points for my experience in resume (Please do not add experince level and NO JSON Array) , give me result in HTML tags as li of ul'
+const PROMPT='position titile: {positionTitle} , Depends on position title give me 5-7 bullet points for my experience in resume (Please do not add experince level and NO JSON Array) , only give me result in HTML tags format'
 
 function RichTextEditor({onRichTextEditorChange,index,defaultValue}) {
     const [value,setValue]=useState(defaultValue);
