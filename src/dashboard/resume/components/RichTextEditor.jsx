ea11278c622a941a8ca48933c50d5e5b641cb023
Rahul Yadav <98537578+therahulyadav@users.noreply.github.com>
2024-11-18 04:32:48 -0800

    Update RichTextEditor.jsx

--- src/dashboard/resume/components/RichTextEditor.jsx
+++ src/dashboard/resume/components/RichTextEditor.jsx
@@ -7,7 +7,7 @@ import { AIChatSession } from './../../../../service/AIModal';
 import { toast } from 'sonner';
 import { motion } from 'framer-motion';
 
-const PROMPT='position titile: {positionTitle} , Depends on position title give me 5-7 bullet points for my experience in resume (Please do not add experince level and give bulletPoints with can be map) , give me result in HTML tags'
+const PROMPT='position titile: {positionTitle} , Depends on position title give me 5-7 bullet points for my experience in resume (Please do not add experince level and NO JSON Array) , give me result in HTML tags as li of ul'
 
 function RichTextEditor({onRichTextEditorChange,index,defaultValue}) {
     const [value,setValue]=useState(defaultValue);
@@ -25,11 +25,7 @@ function RichTextEditor({onRichTextEditorChange,index,defaultValue}) {
       const result=await AIChatSession.sendMessage(prompt);
       console.log(result.response.text());
       const resp=result.response.text();
-      const res = resp.replace('[','').replace(']','');
-      const data = res.bulletPoints.map((val)=>{
-          return(<li>{val}</li>)
-      });
-      setValue(<ul>{data}</ul>);
+      setValue(resp);
       setLoading(false);
     }
   
